6ac0b376e5fca9bc6bf7d6609c3ea71876bd7c0d
alishaw123 <51371505+alishaw123@users.noreply.github.com>
2019-06-04 09:43:40 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+    $('button').click(function(){
+    var name = $('input ');
+    var age = $('.age');
+    var mood = $('mood');
+    $(".answer").text('<p>' +name +age+mood+"You will be prosperous and live a good life.");
+});    
+   
+    
